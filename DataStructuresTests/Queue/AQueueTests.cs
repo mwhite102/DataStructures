724b45ec4b6124450513db3c6164bdda0aaacad4
mwhite102 <mwhite102@gmail.com>
2016-12-21 05:29:14 -0600

    Added Stack and Queue implementations and associated unit test

---
+++ DataStructuresTests/Queue/AQueueTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Queue.Tests
+{
+    [TestClass()]
+    public class AQueueTests
+    {
+        private AQueue<int> InitQueue()
+        {
+            AQueue<int> queue = new AQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            return queue;
+        }
+
+        [TestMethod()]
+        public void Should_Enqueue_Two_Items_Test()
+        {
+            int expectedCount = 2;
+            AQueue<int> queue = InitQueue();
+            Assert.AreEqual(queue.Count, expectedCount);
+        }
+
+        [TestMethod()]
+        public void Should_Enqueue_Two_Items_And_Dequeue_First_Item_Test()
+        {
+            int expectedValue = 1;
+            int expectedCount = 1;
+
+            AQueue<int> queue = InitQueue();
+
+            Assert.AreEqual(queue.Dequeue(), expectedValue);
+            Assert.AreEqual(queue.Count, expectedCount);
+        }
+
+        [TestMethod()]
+        public void Should_Enqueue_Two_Items_And_Peek_First_Item_Test()
+        {
+            int expectedValue = 1;
+            int expectedCount = 2;
+
+            AQueue<int> queue = InitQueue();
+
+            Assert.AreEqual(queue.Peek(), expectedValue);
+            Assert.AreEqual(queue.Count, expectedCount);
+        }
+    }
+}
