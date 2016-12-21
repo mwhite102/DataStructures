061e88d7440015cd934bc7a362c8f7ff9867d4eb
mwhite102 <mwhite102@gmail.com>
2016-12-21 07:51:02 -0600

    Added Deque implementation and associated unit test

---
+++ DataStructures/Deque/ADeque.cs
@@ -0,0 +1,88 @@
+﻿using DataStructures.DoublyLinkedList;
+using System;
+
+namespace DataStructures.Deque
+{
+    public class ADeque<T>
+    {
+        // Backed by a Doubly Linked List
+        private DoublyLinkedList<T> _Items = new DoublyLinkedList<T>();
+
+        /// <summary>
+        /// Adds an item to the head of the queue
+        /// </summary>
+        /// <param name="value"></param>
+        public void EnqueueFirst(T value)
+        {
+            _Items.AddFirst(value);
+        }
+
+        /// <summary>
+        /// Adds an items to the tail of the queue
+        /// </summary>
+        /// <param name="value"></param>
+        public void EnqueueLast(T value)
+        {
+            _Items.AddLast(value);
+        }
+
+        /// <summary>
+        /// Removes an item from the head of the queue
+        /// </summary>
+        /// <returns>The first item from the head of the queue</returns>
+        public T DequeueFirst()
+        {
+            if (Count == 0) throw new InvalidOperationException();
+
+            T value = _Items.Head.Value;
+            _Items.RemoveFirst();
+            return value;
+        }
+
+        /// <summary>
+        /// Removes an item from the tail of the queue
+        /// </summary>
+        /// <returns>The first item from the tail of the queue</returns>
+        public T DequeueLast()
+        {
+            if (Count == 0) throw new InvalidOperationException();
+
+            T value = _Items.Tail.Value;
+            _Items.RemoveLast();
+            return value;
+        }
+
+        /// <summary>
+        /// Returns the first item from the head of the queue, but does not remove it
+        /// </summary>
+        /// <returns>The first item from the head of the queue</returns>
+        public T PeekFirst()
+        {
+            if (Count == 0) throw new InvalidOperationException();
+
+            return _Items.Head.Value;
+        }
+
+        /// <summary>
+        /// Returns the first item from the tail of the queue, but does not remove it
+        /// </summary>
+        /// <returns>The first item from the tail of the queue</returns>
+        public T PeekLast()
+        {
+            if (Count == 0) throw new InvalidOperationException();
+
+            return _Items.Tail.Value;
+        }
+
+        /// <summary>
+        /// The number of items in the Deque
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _Items.Count;
+            }
+        }
+    }
+}
---
+++ DataStructuresTests/Deque/ADequeTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures.Deque;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Deque.Tests
+{
+    [TestClass()]
+    public class ADequeTests
+    {
+        private ADeque<int> InitDeque()
+        {
+            ADeque<int> deque = new ADeque<int>();
+
+            deque.EnqueueFirst(1);
+            deque.EnqueueFirst(2);
+            deque.EnqueueFirst(3);
+            deque.EnqueueFirst(4);
+
+            return deque;
+        }
+
+        [TestMethod()]
+        public void Enqueue_First_Item_Test()
+        {
+            int expectedValue = 0;
+            int expectedCount = 5;
+
+            ADeque<int> deque = InitDeque();
+
+            deque.EnqueueFirst(0);
+
+            Assert.AreEqual(deque.PeekFirst(), expectedValue);
+            Assert.AreEqual(deque.Count, expectedCount);
+        }
+
+        [TestMethod()]
+        public void Enqueue_Last_Item_Test()
+        {
+            int expectedValue = 0;
+            int expectedCount = 5;
+
+            ADeque<int> deque = InitDeque();
+
+            deque.EnqueueLast(0);
+
+            Assert.AreEqual(deque.PeekLast(), expectedValue);
+            Assert.AreEqual(deque.Count, expectedCount);
+        }
+
+        [TestMethod()]
+        public void Dequeue_First_Item_Test()
+        {
+            int expectedValue = 4;
+            int expectedCount = 3;
+
+            ADeque<int> deque = InitDeque();
+
+            int value = deque.DequeueFirst();
+
+            Assert.AreEqual(value, expectedValue);
+            Assert.AreEqual(deque.Count, expectedCount);
+        }
+
+        [TestMethod()]
+        public void Dequeue_Last_Item_Test()
+        {
+            int expectedValue = 1;
+            int expectedCount = 3;
+
+            ADeque<int> deque = InitDeque();
+
+            int value = deque.DequeueLast();
+
+            Assert.AreEqual(value, expectedValue);
+            Assert.AreEqual(deque.Count, expectedCount);
+        }
+    }
+}
